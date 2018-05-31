0f133d09412869f497b0e0ad48d179c5bce0b7dc
Aditya Kaushik <adityakaushik471@gmail.com>
2018-05-31 15:59:06 +0530

    Add model and forms

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
-
+from django.urls import reverse
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeginKey('auth.User') # Connecting it directly to the superuser(one to many type)
+    title = models.CharField(max_length=256)
+    text = models.CharField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approved_comment(self):
+        self.comments.filter(approved_comment=True)
+
+    def __str__(self):
+        return self.title()
+
+    def get_absolute_url(request):
+        return reverse('post_detail', kwargs={'pk': self.pk,})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=256)
+    text = models.CharField()
+    create_date = models.DateTimeField(default=imezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
