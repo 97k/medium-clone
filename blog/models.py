1651eb0310a4b5c55bbd834e222ff3ae795c3805
Aditya Kaushik <adityakaushik471@gmail.com>
2018-06-01 22:43:21 +0530

    First Release

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeginKey('auth.User') # Connecting it directly to the superuser(one to many type)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE) # Connecting it directly to the superuser(one to many type)
     title = models.CharField(max_length=256)
-    text = models.CharField()
-    create_date = models.DateTimeField(default=timezone.now())
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
@@ -17,25 +17,25 @@ class Post(models.Model):
     def approved_comment(self):
         self.comments.filter(approved_comment=True)
 
-    def get_absolute_url(request):
+    def get_absolute_url(self):
         return reverse('post_detail', kwargs={'pk': self.pk,})
 
     def __str__(self):
-        return self.title()
+        return self.title
 
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=256)
-    text = models.CharField()
-    create_date = models.DateTimeField(default=imezone.now())
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
         self.approved_comment = True
         self.save()
 
-    def get_absolute_url(request):
+    def get_absolute_url(self):
         return reverse('post_list', kwargs={'pk': self.pk,})
 
     def __str__(self):
